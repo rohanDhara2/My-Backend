654c1d789662c1af2383800718a04b50c1c8bdd9
“rohanDhara” <dhara.rohan132@gmail.com>
2024-05-26 20:16:48 +0530

    add multer and cloudinary files for upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudnary = async (filePath) => {
+    try {
+        if(!filePath) return null;
+        const response = await cloudinary.uploader.upload(filePath,{
+            resource_type: 'auto'
+        })
+        //file has been uploaded successfully
+        console.log("File uploaded on cloudinary ", response.url);
+        return response
+    } catch (error) {
+        fs.unlinkSync(filePath) //remove the locally saved temp file as the upload operation failed
+        return null;
+    }
+}
+
+
+
+
+
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
