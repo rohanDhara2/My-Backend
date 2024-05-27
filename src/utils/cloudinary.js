7efc0ed48311db9b196012126a6db7ca7ac48faa
“rohanDhara” <dhara.rohan132@gmail.com>
2024-05-27 15:42:25 +0530

    for file upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -22,11 +22,6 @@ const uploadOnCloudnary = async (filePath) => {
     }
 }
 
-
-
-
-
-
 cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
   { public_id: "olympic_flag" }, 
   function(error, result) {console.log(result); });
